9ee18442c806b0b3b43d00ebffc256b9b987206a
Lightning D. Edge <rafiki.gameiro@gmail.com>
2019-11-01 19:32:31 +0000

    TP1 phase 1
    Comments added; report needed, jar signed needed

--- TP1/src/secureProtocol/Logger.java
+++ TP1/src/secureProtocol/Logger.java
@@ -10,10 +10,11 @@ import com.google.gson.reflect.TypeToken;
 public class Logger {
 
     static final String FILE_NOT_FOUND = "The config File does not exist.";
+    static final String IO_EXCEPTION = "An error occurred during the creation o the writing of the log.";
     static final String FILE_LOCATION = "/secureProtocol/Config/MessageLog.conf";
 
-    static final String CHAT_JOIN_MESSAGE ="NOVO PARTICIPANTE: %s juntou-se ao grupo do chat.";
-    static final String CHAT_LEAVE_MESSAGE ="ABANDONO: %s abandonou o grupo de chat.";
+    static final String CHAT_JOIN_MESSAGE = "NOVO PARTICIPANTE: %s juntou-se ao grupo do chat.";
+    static final String CHAT_LEAVE_MESSAGE = "ABANDONO: %s abandonou o grupo de chat.";
 
     static final int JOIN = 1;
     static final int LEAVE = 2;
@@ -27,6 +28,10 @@ public class Logger {
         getLogs();
     }
 
+    /**
+     * Retrieves the log from the last access to the chat room
+     * If the file containing the logs was not found or correctly open an FileNotFoundException is thrown
+     */
     private void getLogs() {
         FileReader fr;
         try {
@@ -34,7 +39,8 @@ public class Logger {
             JsonArray log = (new Gson()).fromJson(fr, JsonObject.class).getAsJsonArray(address);
 
             Gson gson = new Gson();
-            messages = gson.fromJson(log, new TypeToken<LinkedList<String>>() {}.getType());
+            messages = gson.fromJson(log, new TypeToken<LinkedList<String>>() {
+            }.getType());
 
         } catch (FileNotFoundException e) {
             System.err.println(FILE_NOT_FOUND);
@@ -42,6 +48,16 @@ public class Logger {
 
     }
 
+    /**
+     * Adds the message into the messages list
+     * If it is a join message or leave message, a default message template is used to generate the entry
+     * If it is a leave message, the program will store the messages array in a file in order to preserve the registry
+     *
+     * @param user           sender of the message
+     * @param sequenceNumber sequence number the current message has
+     * @param payload        plaintext of the received message
+     * @throws IOException If some error happens with the creation of the DataInputStream
+     */
     protected void addMessage(String user, int sequenceNumber, byte[] payload) throws IOException {
 
         int code = 0;
@@ -53,7 +69,7 @@ public class Logger {
             instream.readUTF();
 
             message = instream.readUTF();
-        }catch (EOFException e) {
+        } catch (EOFException e) {
             switch (code) {
                 case JOIN:
                     message = String.format(CHAT_JOIN_MESSAGE, user);
@@ -71,6 +87,11 @@ public class Logger {
             storeLogs();
     }
 
+    /**
+     * Stores the messages list into the config file in order to preserve the registry
+     * If the file was not found an FileNotFoundException is thrown
+     * If some error happens during the writing in the file, an IOException is thrown
+     */
     private void storeLogs() {
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -93,7 +114,7 @@ public class Logger {
         } catch (FileNotFoundException e) {
             System.err.println(FILE_NOT_FOUND);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println(IO_EXCEPTION);
         }
     }
 }
--- TP1/src/secureProtocol/SMCPSocket.java
+++ TP1/src/secureProtocol/SMCPSocket.java
@@ -16,7 +16,6 @@ import com.google.gson.JsonObject;
 
 import exceptions.ReplayingMessageException;
 import exceptions.TamperedMessageException;
-import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
 
 public class SMCPSocket extends MulticastSocket {
@@ -80,6 +79,12 @@ public class SMCPSocket extends MulticastSocket {
     }
 
     @Override
+    /**
+     * Checks if the typed address and port match some chat entry
+     * If it does, loads a log stored in memory will all the previous messages
+     * And it also creates a stream to the keystore in order to access the keys needed to use the algorithm
+     * associated with the current chat room
+     */
     public void joinGroup(InetAddress mcastaddr) throws IOException {
         try {
             String address = mcastaddr.getHostAddress() + ":" + chatID;
@@ -106,6 +111,19 @@ public class SMCPSocket extends MulticastSocket {
     }
 
     @Override
+    /**
+     * Creates a new packet to be sent to the users in the chat room
+     * The methods goes through the following process
+     * Inserts inside a byte stream the version of the protocol, the chat identifier, the message type code and
+     * the attributes associated with the chat room, needed to perform the security
+     * After that, encrypts the payload using symmetric cryptography and puts inside the byte stream
+     * To do that, the program will generate a random number between 0 and the length of the constant string array ENCODE_MODE
+     * With that, the program will generate an IV which will be used to encrypt the payload
+     * The position of the array used to generate the IV is put in the byte stream,
+     * as the length of the new payload and payload itself
+     * Finally, a MAC is generated in order to provide some integrity prove and with the data for the datagram packet is generated
+     * The method super.send() is called and the packet is send to the other users in the chat, including the local user.
+     */
     public void send(DatagramPacket p) throws IOException {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         DataOutputStream dataStream = new DataOutputStream(byteStream);
@@ -140,18 +158,19 @@ public class SMCPSocket extends MulticastSocket {
     }
 
     @Override
+    /**
+     * calls super.receive(message) to receive the message
+     * checks if the message computed hash is the same as the integrity hash
+     * if it is, gets the secure payload by decrypting it
+     * checks if the payload hash is the same as the previously computed
+     * After that, verifies if the received message is a duplicated ou a replying attack
+     * If it is, the message will not be processed, but if it is not, it will copy the information
+     * into the buffer inside the datagram packet
+     */
     public void receive(DatagramPacket p) throws IOException {
-
-        //checks if the message hash is the same as the integrity hash
-        //if it is, gets the secure payload
-        //does the cryptography
-        //checks if the payload hash is the same as the previously computed
-        //if it is, returns the message
-        //calls super.receive(message)
 
         super.receive(p);
         DataInputStream instream = new DataInputStream(new ByteArrayInputStream(p.getData(), p.getOffset(), p.getLength()));
-
         try {
             checkMessageIntegrity(p.getData());
 
@@ -178,6 +197,22 @@ public class SMCPSocket extends MulticastSocket {
 
     }
 
+    /**
+     * Checks if the received packet as tampered
+     * It places all the information in the packet except the mac, inside a byte stream
+     * With that, computes the respective mac and compares the two of them
+     * If both are the same, the message was not tampered
+     * The method also checks if the other parameters inside the packet have the values they are supposed to have
+     * If some parameter has a different value, the program assumes it was changed through a tampered attack
+     *
+     * @param data data contained inside the packet
+     * @throws IOException               If some error happens with the creation of the streams
+     * @throws UnrecoverableKeyException If the key in the keystore can no longer be retrieved
+     * @throws NoSuchAlgorithmException  If the algorithm name used to generate the MAC does not exist
+     * @throws KeyStoreException         If the keystore does not have an entry with such alias
+     * @throws InvalidKeyException       If the inserted key does not have a specific size or belongs to a different algorithm
+     * @throws TamperedMessageException  If the message suffered a tampered attack
+     */
     private void checkMessageIntegrity(byte[] data) throws IOException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, InvalidKeyException, TamperedMessageException {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         DataInputStream instream = new DataInputStream(new ByteArrayInputStream(data, 0, data.length));
@@ -219,6 +254,17 @@ public class SMCPSocket extends MulticastSocket {
             throw new TamperedMessageException(TAMPERED_MESSAGE);
     }
 
+    /**
+     * Checks if the received payload is the same as the one created by the sender
+     * The verification is made by generating an hash will the plaintext and
+     * comparing it with the hash the sender computed during the send process
+     *
+     * @param data data corresponding to the payload
+     * @return the original payload without cryptography and extra parameters
+     * @throws NoSuchAlgorithmException If the algorithm name used to generate the hash does not exist
+     * @throws IOException              If some error happens with the creation of the streams
+     * @throws TamperedMessageException If the payload was tampered
+     */
     private byte[] checkPayloadIntegrity(byte[] data) throws NoSuchAlgorithmException, IOException, TamperedMessageException {
 
         DataInputStream instream = new DataInputStream(new ByteArrayInputStream(data, 0, data.length));
@@ -242,6 +288,15 @@ public class SMCPSocket extends MulticastSocket {
         return payload;
     }
 
+    /**
+     * Checks in the hash map if the sender's entry has a value equal or superior to the one in the current message
+     * If the user is not registed yet, in the map, is created an entry with the value being the sequence number
+     * inside the message
+     * If the sender already has an entry in the map, checks if the sequence number in the current message
+     * is equal or inferior to the one registered in the map, and if it is throws a ReplayingMessageException
+     *
+     * @throws ReplayingMessageException if the current processed message originated from a replaying attack
+     */
     private void messageAlreadyReceived() throws ReplayingMessageException {
         if (lastMessageReceived.containsKey(destUser)) {
             if (lastMessageReceived.get(destUser) < destSeqNumb)
@@ -252,6 +307,13 @@ public class SMCPSocket extends MulticastSocket {
             lastMessageReceived.put(destUser, destSeqNumb);
     }
 
+    /**
+     * Accesses the chat configuration settings in the config file in the Config folder
+     * If the the file does not exist or was not correctly accessed, throws a FileNotFoundException
+     *
+     * @param address address typed by the user in order to access the chat
+     * @return true if the parameters were obtained, false otherwise
+     */
     private boolean chatAuthentication(String address) {
 
         FileReader fr;
@@ -279,7 +341,13 @@ public class SMCPSocket extends MulticastSocket {
         return true;
     }
 
-    private byte[] computeSessionAttr() throws NoSuchAlgorithmException, NullPointerException {
+    /**
+     * Generates an hash with attributes related to current chat's encryption
+     *
+     * @return the hash computed in the MAC
+     * @throws NoSuchAlgorithmException If the algorithm name used to generate the hash does not exist
+     */
+    private byte[] computeSessionAttr() throws NoSuchAlgorithmException {
 
         if (sessionAttr != null)
             return sessionAttr;
@@ -298,6 +366,26 @@ public class SMCPSocket extends MulticastSocket {
         return sessionAttr;
     }
 
+    /**
+     * Computes the secure payload of the packet to send
+     * The method will store in a byte stream, the username, the sequence number of the message, a nonce, the plaintext
+     * and the hash, computed with the plain text
+     * After inserting all those elements in the stream, the byte array that has the elements will be encrypted in order to
+     * protect the packet against possible attackers
+     *
+     * @param payload original plaintext
+     * @param encode  integer that corresponds to a position in the array ENCODE_MODE
+     * @return the new payload, with more fields and encrypted
+     * @throws UnrecoverableKeyException          If the key in the keystore can no longer be retrieved
+     * @throws NoSuchAlgorithmException           If the algorithm name used to do the symmetric cryptography does not exist
+     * @throws KeyStoreException                  If the keystore does not have an entry with such alias
+     * @throws NoSuchPaddingException             If the program did no specify the padding to do the cryptography
+     * @throws BadPaddingException                If the program inserted a padding that does not exist for that algorithm instance
+     * @throws IllegalBlockSizeException          If the inserted block size is too long or too short for the specified algorithm
+     * @throws InvalidAlgorithmParameterException If the parameters inserted are invalid for the specified algorithm
+     * @throws InvalidKeyException                If the inserted key does not have a specific size or belongs to a different algorithm
+     * @throws IOException                        If some error happens with the creation of the streams
+     */
     private byte[] computePayload(byte[] payload, int encode) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException, IOException {
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
         DataOutputStream dataStream = new DataOutputStream(byteStream);
@@ -326,6 +414,12 @@ public class SMCPSocket extends MulticastSocket {
         return data;
     }
 
+    /**
+     * Gets the the nickname of the local user and stores it
+     *
+     * @param message message to be processed
+     * @throws IOException If some error happens with the creation of the DataInputStream
+     */
     private void getUsername(byte[] message) throws IOException {
         DataInputStream istream = new DataInputStream(new ByteArrayInputStream(message, 0, message.length));
 
@@ -334,6 +428,31 @@ public class SMCPSocket extends MulticastSocket {
         username = istream.readUTF();
     }
 
+    /**
+     * Encrypts the new generated payload using symmetric cryptography
+     * Firstly, it will access the keystore the retrieve the key needed to perform the cryptography
+     * After that it will generate the IV used to start the cryptography
+     *
+     * The generation of the IV e somewhat complicated
+     * All the users that are in the chat room have a default IV, but in order to give some security
+     * its bytes are tampered in order to generate new IV from the original IV
+     *
+     * With that, the program will perform the cryptography
+     * If the algorithm used to encrypt/decrypt is GCM, a different ParameterSpec object will be generated
+     *
+     * @param mode operation the method will executing. It can encrypt or decrypt
+     * @param encode integer that corresponds to a position in the array ENCODE_MODE
+     * @param data new payload with the plaintext and other additional fields
+     * @return the data encrypted
+     * @throws UnrecoverableKeyException If the key in the keystore can no longer be retrieved
+     * @throws NoSuchAlgorithmException If the algorithm name used to do the symmetric cryptography does not exist
+     * @throws KeyStoreException If the keystore does not have an entry with such alias
+     * @throws NoSuchPaddingException If the program did no specify the padding to do the cryptography
+     * @throws BadPaddingException If the program inserted a padding that does not exist for that algorithm instance
+     * @throws IllegalBlockSizeException If the inserted block size is too long or too short for the specified algorithm
+     * @throws InvalidAlgorithmParameterException If the parameters inserted are invalid for the specified algorithm
+     * @throws InvalidKeyException If the inserted key does not have a specific size or belongs to a different algorithm
+     */
     private byte[] applyCrypto(String mode, int encode, byte[] data) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
 
         Cipher cipher = Cipher.getInstance(algorithmName + "/" + encryptMode + "/" + padding);
@@ -366,6 +485,16 @@ public class SMCPSocket extends MulticastSocket {
         return cipher.doFinal(data);
     }
 
+    /**
+     * Computes the MAC of the payload, in order to provide a security check
+     *
+     * @param payload array of bytes that will be used to computed the MAC
+     * @return the hash computed in the MAC
+     * @throws UnrecoverableKeyException If the key in the keystore can no longer be retrieved
+     * @throws NoSuchAlgorithmException  If the algorithm name used to generate the MAC does not exist
+     * @throws KeyStoreException         If the keystore does not have an entry with such alias
+     * @throws InvalidKeyException       If the inserted key does not have a specific size or belongs to a different algorithm
+     */
     private byte[] generateMAC(byte[] payload) throws UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, InvalidKeyException {
 
         if (macName.equalsIgnoreCase("DES"))
@@ -381,6 +510,16 @@ public class SMCPSocket extends MulticastSocket {
         return mac.doFinal();
     }
 
+    /**
+     * Generates the password associated with a keystore entry
+     * Computes the hash of the title of the chat and add the type of keystore entry wanted
+     * It might me a symmetric algorithm or a MAC
+     * After computing the hash, cast its value into string and returns a substring which is the keystore entry's password
+     *
+     * @param type type of keystore entry
+     * @return password of keystore entry
+     * @throws NoSuchAlgorithmException if the algorithm used to computed the hash does not exist
+     */
     private String getPassword(String type) throws NoSuchAlgorithmException {
 
         MessageDigest hash = MessageDigest.getInstance(hashName);
